fb0514fe5ba3d53d644b87e962dc3acfb2d66050
PratikDAvhad <zeninvictor9@gmail.com>
2026-08-01 15:26:59 +0530

    Update sendEmail.js

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,27 +3,40 @@ require("dotenv").config();
 
 console.log("BREVO_EMAIL:", process.env.BREVO_EMAIL);
 console.log("BREVO_API_KEY exists:", !!process.env.BREVO_API_KEY);
+
 const transporter = nodemailer.createTransport({
-  
   host: "smtp-relay.brevo.com",
   port: 587,
+  secure: false,
+  requireTLS: true,
+
   auth: {
     user: process.env.BREVO_EMAIL,
     pass: process.env.BREVO_API_KEY,
   },
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 const sendEmail = async (to, subject, text) => {
-  console.log("Before SMTP verify");
+  console.log("Before SMTP send");
 
-  const info = await transporter.sendMail({
-    from: `"Tango" <${process.env.BREVO_EMAIL}>`,
-    to,
-    subject,
-    text,
-  });
+  try {
+    const info = await transporter.sendMail({
+      from: `"Tango" <${process.env.BREVO_EMAIL}>`,
+      to,
+      subject,
+      text,
+    });
 
-  console.log("Email sent:", info);
+    console.log("Email sent successfully:", info.messageId);
+    return info;
+  } catch (err) {
+    console.error("SMTP SEND ERROR:", err);
+    throw err;
+  }
 };
 
 module.exports = sendEmail;
