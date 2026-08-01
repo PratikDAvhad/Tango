220d94b709743f9dc5e7b53e36758940c25c04ae
PratikDAvhad <zeninvictor9@gmail.com>
2026-08-01 15:37:26 +0530

    Email verification issue

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,22 +1,33 @@
-const nodemailer = require("nodemailer");
+const axios = require("axios");
 require("dotenv").config();
 
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  auth: {
-    user: process.env.BREVO_EMAIL,
-    pass: process.env.BREVO_API_KEY,
-  },
-});
-
 const sendEmail = async (to, subject, text) => {
-  await transporter.sendMail({
-    from: `"Tango" <${process.env.SENDER_EMAIL}>`,
-    to,
-    subject,
-    text,
-  });
+  try {
+    const response = await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          name: "Tango",
+          email: "avhadpratik938@gmail.com",
+        },
+        to: [{ email: to }],
+        subject,
+        textContent: text,
+      },
+      {
+        headers: {
+          "api-key": process.env.BREVO_API_KEY,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    console.log("Brevo email sent:", response.data);
+    return response.data;
+  } catch (err) {
+    console.error("Brevo API Error:", err.response?.data || err.message);
+    throw err;
+  }
 };
 
 module.exports = sendEmail;
